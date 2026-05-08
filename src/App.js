9ffaae54eca622455d213df24e62b556a5bfe46f
Pratite Acharya <pratitea2007@gmail.com>
2026-05-08 14:17:51 +0530

    Completed ShopKart Routing Assignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Route } from "react-router-dom";
 // TODO: Import About
 // TODO: Import Contact
 // TODO: Import React Router
+import { BrowserRouter as Router } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
 
 function App() {
   return (
     <div>
       <Navbar />
-      
+      <Router>  </Router>
       {/* TODO: Implement Routing Here */}
-      
-      <Home />
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav>
       {/* TODO: Replace <a> with Link */}
-      
-      <a href="/">Home</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
-      
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </nav>
   );
 }
